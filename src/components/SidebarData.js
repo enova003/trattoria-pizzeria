8796f89911a838a677b34b7df554fd73dcc2e74d
Ethan Novak <enova003@odu.edu>
2024-07-29 22:58:36 -0500

    added sidebar and pages

---
+++ src/components/SidebarData.js
@@ -0,0 +1,22 @@
+export const SidebarData = [
+    {
+        title: "Home",
+        path: "./pages/",
+    },
+    {
+        title: "Menu",
+        path: "./pages/menu",
+    },
+    {
+        title: "About",
+        path: "./pages/about",
+    },
+    {
+        title: "Order Online",
+        path: "https://www.dapperdeliveries.com/r/691/restaurants/delivery/Pizza-Pasta/Trattoria-Pizza-Italian-Spanish-Fort",
+    },
+    {
+        title: "Pizza Quiz",
+        path: "./pages/pizza-quiz",
+    },
+];
---
+++ src/components/SubMenu.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const SidebarLink = styled(Link)`
+    display: flex;
+    color: #e1e9fc;
+    justify-content: space-between;
+    align-items: center;
+    padding: 20px;
+    list-style: none;
+    height: 60px;
+    text-decoration: none;
+    font-size: 18px;
+
+    &:hover {
+        background: #252831;
+        border-left: 4px solid red;
+        cursor: pointer;
+    }
+`;
+
+const SidebarLabel = styled.span`
+    margin-left: 16px;
+`;
+
+const SubMenu = ({ item }) => {
+    const [subnav, setSubnav] = useState(false);
+
+    const showSubnav = () => setSubnav(!subnav);
+
+    return (
+        <>
+            <SidebarLink
+                to={item.path}
+                onClick={item.subNav && showSubnav}
+            >
+                <div>
+                    <SidebarLabel>
+                        {item.title}
+                    </SidebarLabel>
+                </div>
+            </SidebarLink>
+        </>
+    );
+};
+
+export default SubMenu;
