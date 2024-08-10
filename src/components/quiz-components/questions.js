b3cd87414a4fd1f5a8ee5d3d828fb83491ca7fad
Ethan Novak <enova003@odu.edu>
2024-08-10 10:23:40 -0500

    Updated quiz components

--- src/components/quiz-components/questions.js
+++ src/components/quiz-components/questions.js
@@ -2,43 +2,48 @@
 // 1. MCQs | Multiple Choice | single
 
 export const quiz = {
-    topic: 'Pizza Quiz',
-    /* level: 'Beginner', */
-    totalQuestions: 5,
-    /* perQuestionScore: 5, */
-    questions: [
-      {
-        question: 'What type of sauce do you prefer as the base?',
-        choices: ['Red sauce', 'Alfredo sauce', 'BBQ sauce', 'Buffalo sauce'],
-/*         type: 'MCQs',
-        correctAnswer: 'stringify()', */
-      },
-      {
-        question: 'What is your ideal topping combination?',
-        choices: ['Lots of cheese', 'A variety of meats', 'Lots of veggies', 'A mix of veggies and meat'],
-/*         type: 'MCQs',
-        correctAnswer: 'var and let', */
-      },
-      {
-        question:
-          'How do you feel about spicy or hot flavors?',
-        choices: ['Not a fan', 'I love it spicy'],
-/*         type: 'MCQs',
+  topic: "Pizza Quiz",
+  totalQuestions: 5,
+  questions: [
+    {
+      question: "What type of sauce do you prefer as the base?",
+      choices: ["Red sauce", "Alfredo sauce", "BBQ sauce", "Buffalo sauce"],
+      /* type: 'MCQs',
+        correctAnswer: 'const', */
+    },
+    {
+      question: "What is your ideal topping combination?",
+      choices: [
+        "Lots of cheese",
+        "A variety of meats",
+        "Lots of veggies",
+        "A mix of veggies and meat",
+      ],
+      /*type: 'MCQs',
+        correctAnswer: 'const', */
+    },
+    {
+      question: "How do you feel about spicy or hot flavors?",
+      choices: ["Not a fan", "I love it spicy"],
+      /*type: 'MCQs',
         correctAnswer: 'All of the above', */
-      },
-      {
-        question: 'What type of meat do you like on your pizza?',
-        choices: ['Chicken', 'Steak', 'Pepperoni', 'No meat for me'],
-/*         type: 'MCQs',
+    },
+    {
+      question: "What type of meat do you like on your pizza?",
+      choices: ["Chicken", "Steak", "Pepperoni", "No meat for me"],
+      /*         type: 'MCQs',
         correctAnswer: 'const', */
-      },
-      {
-        question: 'What type of pizza sounds most appealing?',
-        choices: ['Simple yet delicious', 'Rich and creamy', 'Bold, strong, and flavorful', 'Meaty and interesting'],
-/*         type: 'MCQs',
+    },
+    {
+      question: "What type of pizza sounds most appealing?",
+      choices: [
+        "Simple yet delicious",
+        "Rich and creamy",
+        "Bold, strong, and flavorful",
+        "Meaty and interesting",
+      ],
+      /*         type: 'MCQs',
         correctAnswer: 'const', */
-      },
-    ],
-  }
-
-  
+    },
+  ],
+};
--- src/components/quiz-components/quiz.js
+++ src/components/quiz-components/quiz.js
@@ -4,24 +4,24 @@ import "./quiz.css";
 
 const Quiz = () => {
   const [activeQuestion, setActiveQuestion] = useState(0);
-  const [selectedAnswer, setSelectedAnswer] = useState("");
   const [showResult, setShowResult] = useState(false);
   const [selectedAnswerIndex, setSelectedAnswerIndex] = useState(null);
 
   const [pizzaRecommendation, setPizzaRecommendation] = useState("");
-  const [theBull, setTheBull] = useState(0);
-  const [margherita, setMargherita] = useState(0);
-  const [veggie, setVeggie] = useState(0);
-  const [chickenAlfredo, setChickenAlfredo] = useState(0);
-  const [bbqChicken, setBBQChicken] = useState(0);
-  const [phillySteak, setPhillySteak] = useState(0);
-  const [buffaloChicken, setBuffaloChicken] = useState(0);
-  const [chickenPhilly, setChickenPhilly] = useState(0);
-  const [trattHot, setTrattHot] = useState(0);
-  const [theTrattoria, setTheTrattoria] = useState(0);
+  const [theBull, setTheBull] = useState([0, 0, 0, 0, 0]);
+  const [margherita, setMargherita] = useState([0, 0, 0, 0, 0]);
+  const [veggie, setVeggie] = useState([0, 0, 0, 0, 0]);
+  const [chickenAlfredo, setChickenAlfredo] = useState([0, 0, 0, 0, 0]);
+  const [bbqChicken, setBBQChicken] = useState([0, 0, 0, 0, 0]);
+  const [phillySteak, setPhillySteak] = useState([0, 0, 0, 0, 0]);
+  const [buffaloChicken, setBuffaloChicken] = useState([0, 0, 0, 0, 0]);
+  const [chickenPhilly, setChickenPhilly] = useState([0, 0, 0, 0, 0]);
+  const [trattHot, setTrattHot] = useState([0, 0, 0, 0, 0]);
+  const [theTrattoria, setTheTrattoria] = useState([0, 0, 0, 0, 0]);
+  const [userChoices, setUserChoices] = useState(["", "", "", "", ""]);
 
   const { questions } = quiz;
-  const { question, choices, correctAnswer } = questions[activeQuestion];
+  const { question, choices } = questions[activeQuestion];
 
   const onClickNext = () => {
     setSelectedAnswerIndex(null);
@@ -29,16 +29,415 @@ const Quiz = () => {
       setActiveQuestion((prev) => prev + 1);
     } else {
       setActiveQuestion(0);
+      handleQuestions();
+      determineRecomendation();
       setShowResult(true);
     }
   };
 
   const onAnswerSelected = (answer, index) => {
     setSelectedAnswerIndex(index);
-    if (answer === correctAnswer) {
-      setSelectedAnswer(true);
-    } else {
-      setSelectedAnswer(false);
+
+    setUserChoices((prevChoices) => {
+      const updatedChoices = [...prevChoices];
+      updatedChoices[activeQuestion] = answer;
+      return updatedChoices;
+    });
+  };
+
+  const determineRecomendation = () => {
+    const sumTheBull = theBull.reduce((acc, curr) => acc + curr, 0);
+    const sumMargherita = margherita.reduce((acc, curr) => acc + curr, 0);
+    const sumVeggie = veggie.reduce((acc, curr) => acc + curr, 0);
+    const sumChickenAlfredo = chickenAlfredo.reduce(
+      (acc, curr) => acc + curr,
+      0
+    );
+    const sumBBQ = bbqChicken.reduce((acc, curr) => acc + curr, 0);
+    const sumPhillySteak = phillySteak.reduce((acc, curr) => acc + curr, 0);
+    const sumBuffalo = buffaloChicken.reduce((acc, curr) => acc + curr, 0);
+    const sumChickenPhilly = chickenPhilly.reduce((acc, curr) => acc + curr, 0);
+    const sumTrattHot = trattHot.reduce((acc, curr) => acc + curr, 0);
+    const sumTheTrattoria = theTrattoria.reduce((acc, curr) => acc + curr, 0);
+
+    const pizzaSums = [
+      { name: "The Bull", sum: sumTheBull },
+      { name: "Margherita", sum: sumMargherita },
+      { name: "Veggie", sum: sumVeggie },
+      { name: "Chicken Alfredo", sum: sumChickenAlfredo },
+      { name: "BBQ Chicken", sum: sumBBQ },
+      { name: "Philly Steak", sum: sumPhillySteak },
+      { name: "Buffalo Chicken", sum: sumBuffalo },
+      { name: "Chicken Philly", sum: sumChickenPhilly },
+      { name: "Tratt Hot", sum: sumTrattHot },
+      { name: "The Trattoria", sum: sumTheTrattoria },
+    ];
+
+    const maxSum = Math.max(...pizzaSums.map((pizza) => pizza.sum));
+
+    const topPizzas = pizzaSums
+      .filter((pizza) => pizza.sum === maxSum)
+      .map((pizza) => pizza.name);
+
+    const results =
+      topPizzas.length > 1
+        ? `We recommend you order: ${topPizzas.join(" or ")}`
+        : `We recommend you order: ${topPizzas[0]}`;
+
+    setPizzaRecommendation(results);
+  };
+
+  const handleQuestions = () => {
+    determineQuestionOne();
+    determineQuestionTwo();
+    determineQuestionThree();
+    determineQuestionFour();
+    determineQuestionFive();
+  };
+
+  const determineQuestionOne = () => {
+    if (userChoices[0] === "Red sauce") {
+      setTheBull((prev) => {
+        const newState = [...prev];
+        newState[0] = 1;
+        return newState;
+      });
+
+      setMargherita((prev) => {
+        const newState = [...prev];
+        newState[0] = 1;
+        return newState;
+      });
+
+      setVeggie((prev) => {
+        const newState = [...prev];
+        newState[0] = 1;
+        return newState;
+      });
+
+      setTrattHot((prev) => {
+        const newState = [...prev];
+        newState[0] = 1;
+        return newState;
+      });
+
+      setTheTrattoria((prev) => {
+        const newState = [...prev];
+        newState[0] = 1;
+        return newState;
+      });
+    }
+
+    if (userChoices[0] === "Alfredo sauce") {
+      setChickenAlfredo((prev) => {
+        const newState = [...prev];
+        newState[0] = 1;
+        return newState;
+      });
+
+      setPhillySteak((prev) => {
+        const newState = [...prev];
+        newState[0] = 1;
+        return newState;
+      });
+
+      setChickenPhilly((prev) => {
+        const newState = [...prev];
+        newState[0] = 1;
+        return newState;
+      });
+    }
+
+    if (userChoices[0] === "BBQ sauce") {
+      setBBQChicken((prev) => {
+        const newState = [...prev];
+        newState[0] = 1;
+        return newState;
+      });
+    }
+
+    if (userChoices[0] === "Buffalo sauce") {
+      setBuffaloChicken((prev) => {
+        const newState = [...prev];
+        newState[0] = 1;
+        return newState;
+      });
+    }
+  };
+
+  const determineQuestionTwo = () => {
+    if (userChoices[1] === "Lots of cheese") {
+      setMargherita((prev) => {
+        const newState = [...prev];
+        newState[1] = 1;
+        return newState;
+      });
+    }
+
+    if (userChoices[1] === "A variety of meats") {
+      setTheBull((prev) => {
+        const newState = [...prev];
+        newState[1] = 1;
+        return newState;
+      });
+
+      setBuffaloChicken((prev) => {
+        const newState = [...prev];
+        newState[1] = 1;
+        return newState;
+      });
+
+      setTrattHot((prev) => {
+        const newState = [...prev];
+        newState[1] = 1;
+        return newState;
+      });
+    }
+
+    if (userChoices[1] === "Lots of veggies") {
+      setVeggie((prev) => {
+        const newState = [...prev];
+        newState[1] = 1;
+        return newState;
+      });
+    }
+
+    if (userChoices[1] === "A mix of veggies and meat") {
+      setTheTrattoria((prev) => {
+        const newState = [...prev];
+        newState[1] = 1;
+        return newState;
+      });
+
+      setChickenPhilly((prev) => {
+        const newState = [...prev];
+        newState[1] = 1;
+        return newState;
+      });
+
+      setPhillySteak((prev) => {
+        const newState = [...prev];
+        newState[1] = 1;
+        return newState;
+      });
+
+      setBBQChicken((prev) => {
+        const newState = [...prev];
+        newState[1] = 1;
+        return newState;
+      });
+
+      setChickenAlfredo((prev) => {
+        const newState = [...prev];
+        newState[1] = 1;
+        return newState;
+      });
+    }
+  };
+
+  const determineQuestionThree = () => {
+    if (userChoices[2] === "I love it spicy") {
+      setTrattHot((prev) => {
+        const newState = [...prev];
+        newState[2] = 1;
+        return newState;
+      });
+
+      setBBQChicken((prev) => {
+        const newState = [...prev];
+        newState[2] = 1;
+        return newState;
+      });
+
+      setBuffaloChicken((prev) => {
+        const newState = [...prev];
+        newState[2] = 1;
+        return newState;
+      });
+    }
+
+    if (userChoices[2] === "Not a fan") {
+      setMargherita((prev) => {
+        const newState = [...prev];
+        newState[2] = 1;
+        return newState;
+      });
+
+      setVeggie((prev) => {
+        const newState = [...prev];
+        newState[2] = 1;
+        return newState;
+      });
+
+      setTheBull((prev) => {
+        const newState = [...prev];
+        newState[2] = 1;
+        return newState;
+      });
+
+      setChickenAlfredo((prev) => {
+        const newState = [...prev];
+        newState[2] = 1;
+        return newState;
+      });
+
+      setPhillySteak((prev) => {
+        const newState = [...prev];
+        newState[2] = 1;
+        return newState;
+      });
+
+      setChickenPhilly((prev) => {
+        const newState = [...prev];
+        newState[2] = 1;
+        return newState;
+      });
+
+      setTheTrattoria((prev) => {
+        const newState = [...prev];
+        newState[2] = 1;
+        return newState;
+      });
+    }
+  };
+
+  const determineQuestionFour = () => {
+    if (userChoices[3] === "Chicken") {
+      setBuffaloChicken((prev) => {
+        const newState = [...prev];
+        newState[3] = 1;
+        return newState;
+      });
+
+      setChickenAlfredo((prev) => {
+        const newState = [...prev];
+        newState[3] = 1;
+        return newState;
+      });
+
+      setBBQChicken((prev) => {
+        const newState = [...prev];
+        newState[3] = 1;
+        return newState;
+      });
+
+      setChickenPhilly((prev) => {
+        const newState = [...prev];
+        newState[3] = 1;
+        return newState;
+      });
+    }
+
+    if (userChoices[3] === "Steak") {
+      setPhillySteak((prev) => {
+        const newState = [...prev];
+        newState[3] = 1;
+        return newState;
+      });
+    }
+
+    if (userChoices[3] === "Pepperoni") {
+      setTheBull((prev) => {
+        const newState = [...prev];
+        newState[3] = 1;
+        return newState;
+      });
+
+      setTrattHot((prev) => {
+        const newState = [...prev];
+        newState[3] = 1;
+        return newState;
+      });
+
+      setTheTrattoria((prev) => {
+        const newState = [...prev];
+        newState[3] = 1;
+        return newState;
+      });
+    }
+
+    if (userChoices[3] === "No meat for me") {
+      setMargherita((prev) => {
+        const newState = [...prev];
+        newState[3] = 1;
+        return newState;
+      });
+
+      setVeggie((prev) => {
+        const newState = [...prev];
+        newState[3] = 1;
+        return newState;
+      });
+    }
+  };
+
+  const determineQuestionFive = () => {
+    if (userChoices[4] === "Simple yet delicious") {
+      setVeggie((prev) => {
+        const newState = [...prev];
+        newState[4] = 1;
+        return newState;
+      });
+
+      setMargherita((prev) => {
+        const newState = [...prev];
+        newState[4] = 1;
+        return newState;
+      });
+    }
+
+    if (userChoices[4] === "Rich and creamy") {
+      setChickenAlfredo((prev) => {
+        const newState = [...prev];
+        newState[4] = 1;
+        return newState;
+      });
+
+      setPhillySteak((prev) => {
+        const newState = [...prev];
+        newState[4] = 1;
+        return newState;
+      });
+
+      setChickenPhilly((prev) => {
+        const newState = [...prev];
+        newState[4] = 1;
+        return newState;
+      });
+    }
+
+    if (userChoices[4] === "Bold, strong, and flavorful") {
+      setTrattHot((prev) => {
+        const newState = [...prev];
+        newState[4] = 1;
+        return newState;
+      });
+
+      setBBQChicken((prev) => {
+        const newState = [...prev];
+        newState[4] = 1;
+        return newState;
+      });
+
+      setBuffaloChicken((prev) => {
+        const newState = [...prev];
+        newState[4] = 1;
+        return newState;
+      });
+    }
+
+    if (userChoices[4] === "Meaty and interesting") {
+      setTheTrattoria((prev) => {
+        const newState = [...prev];
+        newState[4] = 1;
+        return newState;
+      });
+
+      setTheBull((prev) => {
+        const newState = [...prev];
+        newState[4] = 1;
+        return newState;
+      });
     }
   };
 
@@ -76,24 +475,16 @@ const Quiz = () => {
                 onClick={onClickNext}
                 disabled={selectedAnswerIndex === null}
               >
-                {activeQuestion === questions.length - 1 ? "Finish" : "Next"}
+                {activeQuestion === questions.length - 1 ? "Results" : "Next"}
               </button>
             </div>
           </div>
         ) : (
           <div className="result">
-            <h3>Result</h3>
+            <h3>Results</h3>
             <p>
-              Total Question: <span>{questions.length}</span>
-            </p>
-            <p>
-              Total Score:<span> example </span>
-            </p>
-            <p>
-              Correct Answers:<span> example</span>
-            </p>
-            <p>
-              Wrong Answers:<span> example</span>
+              Based on your answers, we recommend you order: <br />{" "}
+              {pizzaRecommendation}{" "}
             </p>
           </div>
         )}
